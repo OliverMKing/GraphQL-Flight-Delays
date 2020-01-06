fc8cbafc987736cc4931df8e1264b7817ab76ce9
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-01-05 21:17:38 -0500

    Fixed root routing

--- client/index.js
+++ client/index.js
@@ -23,9 +23,8 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}>
-          <Route path="home" component={Home} />
-        </Route>
+        <Route path="/" component={Home} />
+        <Route path="/" component={App}></Route>
       </Router>
     </ApolloProvider>
   );
